58dcec15c43c1f37bfa1c695420faa140fb5f468
Tom Smith <tom.smith@okta.com>
2021-07-07 10:22:24 -0400

    remove optional attributes

--- config.js
+++ config.js
@@ -16,49 +16,58 @@ var profile = {
 /**
  * SAML Attribute Metadata
  */
-var metadata = [{
-  id: "firstName",
-  optional: false,
-  displayName: 'First Name',
-  description: 'The given name of the user',
-  multiValue: false
-}, {
-  id: "lastName",
-  optional: false,
-  displayName: 'Last Name',
-  description: 'The surname of the user',
-  multiValue: false
-}, {
-  id: "displayName",
-  optional: true,
-  displayName: 'Display Name',
-  description: 'The display name of the user',
-  multiValue: false
-}, {
+
+ var metadata = [{
   id: "email",
   optional: false,
   displayName: 'E-Mail Address',
   description: 'The e-mail address of the user',
   multiValue: false
-},{
-  id: "mobilePhone",
-  optional: true,
-  displayName: 'Mobile Phone',
-  description: 'The mobile phone of the user',
-  multiValue: false
-}, {
-  id: "groups",
-  optional: true,
-  displayName: 'Groups',
-  description: 'Group memberships of the user',
-  multiValue: true
-}, {
-  id: "userType",
-  optional: true,
-  displayName: 'User Type',
-  description: 'The type of user',
-  options: ['Admin', 'Editor', 'Commenter']
 }];
+
+// var metadata = [{
+//   id: "firstName",
+//   optional: false,
+//   displayName: 'First Name',
+//   description: 'The given name of the user',
+//   multiValue: false
+// }, {
+//   id: "lastName",
+//   optional: false,
+//   displayName: 'Last Name',
+//   description: 'The surname of the user',
+//   multiValue: false
+// }, {
+//   id: "displayName",
+//   optional: true,
+//   displayName: 'Display Name',
+//   description: 'The display name of the user',
+//   multiValue: false
+// }, {
+//   id: "email",
+//   optional: false,
+//   displayName: 'E-Mail Address',
+//   description: 'The e-mail address of the user',
+//   multiValue: false
+// },{
+//   id: "mobilePhone",
+//   optional: true,
+//   displayName: 'Mobile Phone',
+//   description: 'The mobile phone of the user',
+//   multiValue: false
+// }, {
+//   id: "groups",
+//   optional: true,
+//   displayName: 'Groups',
+//   description: 'Group memberships of the user',
+//   multiValue: true
+// }, {
+//   id: "userType",
+//   optional: true,
+//   displayName: 'User Type',
+//   description: 'The type of user',
+//   options: ['Admin', 'Editor', 'Commenter']
+// }];
 
 module.exports = {
   user: profile,
